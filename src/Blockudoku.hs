126534c90e75aff63fa802cf6355db96a0f07879
Mikhail Brinchuk <thecentury@gmail.com>
2023-04-21 23:55:23 +0200

    Render the board

--- src/Blockudoku.hs
+++ src/Blockudoku.hs
@@ -3,16 +3,53 @@
 module Blockudoku where
 
 import Control.Lens hiding ((<|), (|>), (:>), (:<))
+import Data.Array
+
+data Cell = Free | Filled
+  deriving stock (Show, Eq)
+
+type Coord = (Int, Int)
+
+type Figure = Array Coord Cell
 
 data Game = Game
-  { _score :: Int }
+  { _score :: Int,
+    _board :: Array Coord Cell,
+    _figures :: Array Int (Maybe Figure) }
+  deriving stock (Show)
 
 makeLenses ''Game
 
-height, width :: Int
+height, width, figuresToPlace :: Int
 height = 9
 width = 9
+figuresToPlace = 3
 
 initGame :: IO Game
 initGame = do
-  return $ Game 0
+  let _board =
+        array ((0, 0), (height - 1, width - 1)) [((i, j), Free) | i <- [0 .. height - 1], j <- [0 .. width - 1]]
+        // [
+          ((0, 0), Filled),
+          ((0, 1), Filled),
+          ((1, 0), Filled),
+          ((1, 1), Filled)
+        ]
+  let _figures = array (0, figuresToPlace - 1) [(i, Nothing) | i <- [0 .. figuresToPlace - 1]]
+  let game = Game
+        { _score = 0,
+          _board = _board,
+          _figures = _figures }
+  return game
+
+indicesInRow :: Int -> [(Int, Int)]
+indicesInRow rowIndex = range ((rowIndex, 0), (rowIndex, width - 1))
+
+rowIndices :: [Int]
+rowIndices = [0 .. height - 1]
+
+rowCells :: Int -> Game -> [Cell]
+rowCells rowIndex game = map (\coord -> (game ^. board) ! coord) $ indicesInRow rowIndex
+
+rows :: Game -> [[Cell]]
+rows game = map (`rowCells` game) rowIndices
--- src/UI.hs
+++ src/UI.hs
@@ -8,10 +8,10 @@ import Brick
   ( App(..), AttrMap, BrickEvent(..), EventM, Widget
   , customMain, neverShowCursor
   , halt
-  , hLimit, vBox, hBox  , padRight, padTop, padAll, Padding(..)
+  , hLimit, vBox, hBox, padRight, padLeft, padTop, padAll, Padding(..)
   , withBorderStyle, str
   , attrMap, withAttr, emptyWidget, AttrName, on, fg
-  , (<+>), attrName)
+  , (<+>), attrName, joinBorders)
 import qualified Brick.Widgets.Border as B
 import qualified Brick.Widgets.Border.Style as BS
 import qualified Brick.Widgets.Center as C
@@ -35,7 +35,7 @@ handleEvent _ = return ()
 drawUI :: Game -> [Widget Name]
 drawUI game =
   [
-    C.center $ drawScore game
+    C.center $ drawGrid game <+> padLeft (Pad 2) (drawScore game)
   ]
 
 drawScore :: Game -> Widget Name
@@ -47,8 +47,36 @@ drawScore game =
   $ padAll 1
   $ str $ show $ game ^. score
 
+drawGrid :: Game -> Widget Name
+drawGrid game =
+  withBorderStyle BS.unicodeRounded
+  $ B.border
+  $ padAll 0
+  $ vBox cellRows
+  where
+    cellRows = map (hBox . map borderedCell) $ rows game
+
+    borderedCell :: Cell -> Widget Name
+    borderedCell = drawCell
+
+    drawCell :: Cell -> Widget Name
+    drawCell Free = withAttr emptyCellAttr $ str "· "
+    drawCell Filled = withAttr filledCellAttr $ str "  "
+
+--- Attributes ---
+
+emptyCellAttr, filledCellAttr :: AttrName
+emptyCellAttr = attrName "emptyCell"
+filledCellAttr = attrName "filledCell"
+
 theMap :: AttrMap
-theMap = attrMap V.defAttr []
+theMap = attrMap V.defAttr
+  [
+    (emptyCellAttr, V.defAttr),
+    (filledCellAttr, V.blue `on` V.blue)
+  ]
+
+--- Main ---
 
 main :: IO ()
 main = do
